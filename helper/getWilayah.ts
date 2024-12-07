31e9d935bc2ab7c576b41df552c60ac879d1de1f
masqomar21 <mqomarudin0480@gmail.com>
2024-12-07 20:06:03 +0700

    feat: host to vercer done

--- helper/getWilayah.ts
+++ helper/getWilayah.ts
@@ -1,29 +1,30 @@
 import fs from "fs";
+import path from "path";
 
 const getProvice = async () => {
-  const data = fs.readFileSync("./wilayah_data/provinsi.json", "utf8");
-
+  const filePath = path.resolve(process.cwd(), "wilayah_data/provinsi.json");
+  const data = fs.readFileSync(filePath, "utf8");
   return JSON.parse(data);
 };
 
 const getCity = async (provinceId: string) => {
-  const data = fs.readFileSync("./wilayah_data/kabupaten.json", "utf8");
+  const filePath = path.resolve(process.cwd(), "wilayah_data/kabupaten.json");
+  const data = fs.readFileSync(filePath, "utf8");
   const parsedData = JSON.parse(data);
-
   return parsedData.filter((city: any) => city.provinsi_id == provinceId);
 };
 
 const getDistrict = async (cityId: string) => {
-  const data = fs.readFileSync("./wilayah_data/kecamatan.json", "utf8");
+  const filePath = path.resolve(process.cwd(), "wilayah_data/kecamatan.json");
+  const data = fs.readFileSync(filePath, "utf8");
   const parsedData = JSON.parse(data);
-
   return parsedData.filter((district: any) => district.kabupaten_id == cityId);
 };
 
 const getVillage = async (districtId: string) => {
-  const data = fs.readFileSync("./wilayah_data/kelurahan.json", "utf8");
+  const filePath = path.resolve(process.cwd(), "wilayah_data/kelurahan.json");
+  const data = fs.readFileSync(filePath, "utf8");
   const parsedData = JSON.parse(data);
-
   return parsedData.filter(
     (village: any) => village.kecamatan_id == districtId
   );
